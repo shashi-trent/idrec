49f4a8945c03bf1272ce013eecfcd5e2f49df89c
shashi-trent <shashitre@gmail.com>
2023-07-14 20:27:56 +0530

    reconciliation logic implemented & related orm methods

---
+++ src/main/java/speed/bite/idrec/mappers/ContactMapper.java
@@ -0,0 +1,46 @@
+package speed.bite.idrec.mappers;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import speed.bite.idrec.enums.LinkPrecedence;
+import speed.bite.idrec.pojos.models.ContactModel;
+
+@Mapper
+public interface ContactMapper {
+
+    @Select("select * from contact where id=#{id}")
+    ContactModel getById(int id);
+
+    @Select("select id from contact where deleted_at is null and linked_id is null and email=#{email} limit 1")
+    Integer getIdByPrimaryEmail(String email);
+
+    @Select("select id from contact where deleted_at is null and linked_id is null and phone_number=#{phoneNumber} limit 1")
+    Integer getIdByPrimaryPhone(String phoneNumber);
+
+    @Select("select distinct linked_id from contact where deleted_at is null and " +
+            "linked_id is not null and email=#{email} limit 1")
+    Integer getLinkedIdByEmail(String email);
+
+    @Select("select distinct linked_id from contact where deleted_at is null and " +
+            "linked_id is not null and phone_number=#{phoneNumber} limit 1")
+    Integer getLinkedIdByPhone(String phoneNumber);
+
+
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    @Insert("insert into contact(email, phone_number, linked_id, link_precedence) " +
+            "values(#{model.email}, #{model.phoneNumber}, #{model.linkedId}, #{model.linkPrecedence})"
+    )
+    void insert(@Param("model") ContactModel contactModel);
+
+    @Update("update contact set linked_id=#{linkedId}, link_precedence=#{linkPrecedence} " +
+                "where id=#{id}")
+    void updateSecondaryLinking(@Param("id") int id, @Param("linkedId") int linkedId,
+                          @Param("linkPrecedence")LinkPrecedence linkPrecedence);
+
+    @Update("update contact set linked_id=#{newLinkedId} where deleted_at is null and linked_id=#{prvLinkedId}")
+    void updateLinkedId(@Param("prvLinkedId") int prvLinkedId, @Param("newLinkedId") int newLinkedId);
+}
---
+++ src/main/java/speed/bite/idrec/services/ContactService.java
@@ -0,0 +1,80 @@
+package speed.bite.idrec.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import speed.bite.idrec.enums.LinkPrecedence;
+import speed.bite.idrec.mappers.ContactMapper;
+import speed.bite.idrec.pojos.models.ContactModel;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactMapper contactMapper;
+
+
+    @Transactional
+    private ContactModel reconcileIdentity(String email, String phoneNumber) {
+        ContactModel emailContact = getEmailPrimaryContact(email);
+        boolean emailNotExists = StringUtils.hasText(email) && Objects.isNull(emailContact);
+
+        ContactModel phoneContact = getPhonePrimaryContact(phoneNumber);
+        boolean phoneNotExists = StringUtils.hasText(phoneNumber) && Objects.isNull(phoneContact);
+
+        ContactModel primaryContact = Optional.ofNullable(emailContact).orElse(phoneContact);
+
+        if(emailNotExists || phoneNotExists) {
+            ContactModel secondaryContact = createAndSaveNewContact(email, phoneNumber,
+                    Objects.isNull(primaryContact) ? null : primaryContact.getId());
+
+            if(Objects.isNull(primaryContact)) {
+                primaryContact = secondaryContact;
+            }
+        } else {
+            if(Objects.nonNull(emailContact) && Objects.nonNull(phoneContact)
+                    && !emailContact.getId().equals(phoneContact.getId())) {
+                primaryContact = emailContact.getCreatedAt().compareTo(phoneContact.getCreatedAt()) < 0
+                        ? emailContact : phoneContact;
+                int secondaryContactId = emailContact.getId().equals(primaryContact.getId())
+                        ? phoneContact.getId() : emailContact.getId();
+
+                contactMapper.updateLinkedId(secondaryContactId, primaryContact.getId());
+                contactMapper.updateSecondaryLinking(secondaryContactId, primaryContact.getId(), LinkPrecedence.Secondary);
+            }
+        }
+
+        return primaryContact;
+    }
+
+    private ContactModel getEmailPrimaryContact(String email) {
+        if(!StringUtils.hasText(email)) return null;
+        Integer primaryIdByEmail = Optional.ofNullable(contactMapper.getIdByPrimaryEmail(email))
+                .orElse(contactMapper.getLinkedIdByEmail(email));
+        return Objects.nonNull(primaryIdByEmail) ? contactMapper.getById(primaryIdByEmail) : null;
+    }
+
+    private ContactModel getPhonePrimaryContact(String phoneNumber) {
+        if(!StringUtils.hasText(phoneNumber)) return null;
+        Integer primaryIdByPhone = Optional.ofNullable(contactMapper.getIdByPrimaryPhone(phoneNumber))
+                .orElse(contactMapper.getLinkedIdByPhone(phoneNumber));
+        return Objects.nonNull(primaryIdByPhone) ? contactMapper.getById(primaryIdByPhone) : null;
+    }
+
+    private ContactModel createAndSaveNewContact(String email, String phoneNumber, Integer primaryId) {
+        ContactModel contactModel = ContactModel.builder()
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .linkedId(primaryId)
+                .linkPrecedence(Objects.isNull(primaryId) ? LinkPrecedence.Primary : LinkPrecedence.Secondary)
+                .build();
+
+        contactMapper.insert(contactModel);
+
+        return contactModel;
+    }
+}
