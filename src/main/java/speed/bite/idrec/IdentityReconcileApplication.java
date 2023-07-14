909f6212c222ca8ad3579e88f277d5114190d36e
shashi-trent <shashitre@gmail.com>
2023-07-14 20:13:55 +0530

    Spring Boot Setup with DataBase configured

---
+++ src/main/java/speed/bite/idrec/IdentityReconcileApplication.java
@@ -0,0 +1,13 @@
+package speed.bite.idrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconcileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityReconcileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/speed/bite/idrec/IdentityReconcileApplicationTests.java
@@ -0,0 +1,13 @@
+package speed.bite.idrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconcileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
